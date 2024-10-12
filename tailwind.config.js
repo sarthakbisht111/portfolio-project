2b72fb753137867516d79efbc3b28d0639904fea
Sarthak <sarthakbisht111@gmail.com>
2024-10-12 12:55:10 +0530

    design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*"],
   theme: {
     extend: {
       colors: {
         // Add custom colors here
         'brand-blue': '#202030',
-        'brand-pink': '#CF8BA9',
+        'brand-pink': '#B6517D',
         'brand-green': '#7D98A1',
         'brand-yellow':'#DEF2C8',
         'brand-red':'#711E30',
-        'brand-darkpink':'#B11156'
+        'brand-darkpink':'#B11156',
+        'gradient-orange': '#EEF3D2',
+        'gradient-cream': '#EEECF1',
+        'grad-brown':'#29465B'
         // 'brand-green': 'rgb(72, 191, 145)',  // RGB example
         // 'custom-dark': 'hsl(210, 12%, 20%)'  // HSL example
       },
       fontFamily: {
-        // Define your custom fonts
-        sans: ['Abril Fatface'],
+        acme: ['Acme', 'sans-serif'],
+        abril: ['Abril Fatface', 'cursive'],
       },
     },
   },
